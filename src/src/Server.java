2b7688581d92302f2a66c21ccbfc87fa1c0d74a1
Zorehan <kadargergo20@gmail.com>
2024-08-27 09:47:53 +0200

    haha

--- src/src/Server.java
+++ src/src/Server.java
@@ -1,2 +1,13 @@
+
+import java.net.*;
 public class Server {
+
+    public static void main(String[] args)throws Exception {
+        ServerSocket welcomeSocket = new ServerSocket(9800);
+        while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+            (new ServerThread(connectionSocket)).start();
+        }
+    }
+
 }
