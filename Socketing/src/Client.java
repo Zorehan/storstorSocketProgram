f784a4ce1db68f2a12dd5bc74b3c92d294da3177
Zorehan <kadargergo20@gmail.com>
2024-08-28 09:10:44 +0200

    funny funny UDP overhaul

--- Socketing/src/Client.java
+++ Socketing/src/Client.java
@@ -3,20 +3,35 @@ import java.net.*;
 import java.util.HashMap;
 
 public class Client {
-    private Socket socket;
+
+    private Socket tcpSocket;
+    private DatagramSocket udpSocket;
     private BufferedReader in;
     private PrintWriter out;
+    private InetAddress serverAddress;
+    private int serverPort;
     private String username;
+    private boolean useTCP;
     private static HashMap<String, String> navneServiceMap = new HashMap<>();
 
-    public Client(String serverAddress, int serverPort, String username) {
+    public Client(String serverAddress, int serverPort, String username, boolean useTCP) {
         this.username = username;
+        this.serverPort = serverPort;
+        this.useTCP = useTCP;
+
         try {
-            System.out.println("Connecting to server...");
-            socket = new Socket(serverAddress, serverPort);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream(), true);
-            System.out.println("Connected to server at " + serverAddress + ":" + serverPort);
+            if (useTCP) {
+                System.out.println("Connecting to TCP server...");
+                tcpSocket = new Socket(serverAddress, serverPort);
+                in = new BufferedReader(new InputStreamReader(tcpSocket.getInputStream()));
+                out = new PrintWriter(tcpSocket.getOutputStream(), true);
+                System.out.println("Connected to TCP server at " + serverAddress + ":" + serverPort);
+            } else {
+                System.out.println("Connecting to UDP server...");
+                udpSocket = new DatagramSocket();
+                this.serverAddress = InetAddress.getByName(serverAddress);
+                System.out.println("Connected to UDP server at " + serverAddress + ":" + serverPort);
+            }
         } catch (IOException e) {
             System.err.println("Error connecting to server: " + e.getMessage());
             e.printStackTrace();
@@ -24,13 +39,17 @@ public class Client {
     }
 
     public void start() {
-        new Thread(new IncomingReader()).start();
+        if (useTCP) {
+            new Thread(new IncomingTCPReader()).start();
+        } else {
+            new Thread(new IncomingUDPReader()).start();
+        }
 
         try (BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))) {
             String message;
             while ((message = consoleReader.readLine()) != null) {
                 if (message.toLowerCase().equals("info")) {
-                    System.out.println("the different users you can connect to: ");
+                    System.out.println("The different users you can connect to: ");
                     System.out.println(navneServiceMap.toString());
                 } else {
                     sendMessage(message);
@@ -44,10 +63,22 @@ public class Client {
 
     public void sendMessage(String message) {
         System.out.println("Sending message: " + message);
-        out.println(username + ": " + message);
+        if (useTCP) {
+            out.println(username + ": " + message);
+        } else {
+            try {
+                String fullMessage = username + ": " + message;
+                byte[] sendBuffer = fullMessage.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress, serverPort);
+                udpSocket.send(sendPacket);
+            } catch (IOException e) {
+                System.err.println("Error sending UDP message: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
     }
 
-    private class IncomingReader implements Runnable {
+    private class IncomingTCPReader implements Runnable {
         @Override
         public void run() {
             String message;
@@ -62,18 +93,37 @@ public class Client {
         }
     }
 
+    private class IncomingUDPReader implements Runnable {
+        @Override
+        public void run() {
+            byte[] receiveBuffer = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+            try {
+                while (true) {
+                    udpSocket.receive(receivePacket);
+                    String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                    System.out.println("Received message: " + message);
+                }
+            } catch (IOException e) {
+                System.err.println("Error reading from UDP server: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         navneServiceMap.put("Alex", "192.168.1.10 8080");
         navneServiceMap.put("Mikkel", "192.168.1.11 8081");
         navneServiceMap.put("Gerg", "10.10.131.241 9566");
 
-        if (args.length != 2) {
-            System.out.println("Usage: java Client <server-username> <client-username>");
+        if (args.length != 3) {
+            System.out.println("Usage: java Client <server-username> <client-username> <1 for TCP | 2 for UDP>");
             return;
         }
 
         String serverUsername = args[0];
         String clientUsername = args[1];
+        boolean useTCP = args[2].equals("1");
 
         String addressPort = navneServiceMap.get(serverUsername);
 
@@ -97,7 +147,7 @@ public class Client {
             return;
         }
 
-        Client client = new Client(ip, port, clientUsername);
+        Client client = new Client(ip, port, clientUsername, useTCP);
         client.start();
     }
 }
